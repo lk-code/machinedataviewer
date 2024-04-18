64b8ae97ff54a4b63de507ea8dc9b42b647181ca
Lars Krämer <lars.kraemer@live.de>
2024-04-18 23:22:04 +0200

    MDV-1: added wap project

--- source/DataViewer/App.xaml.cs
+++ source/DataViewer/App.xaml.cs
@@ -2,13 +2,12 @@
 using System.Data;
 using System.Windows;
 
-namespace DataViewer
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace DataViewer;
 
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
 }
+
