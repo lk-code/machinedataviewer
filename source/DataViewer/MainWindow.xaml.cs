2fb33fcba930f40453a4bf8e37c83b25030f74ad
Lars Krämer <lars.kraemer@live.de>
2024-04-19 23:30:51 +0200

    MDV-7: added drag drop logic

--- source/DataViewer/MainWindow.xaml.cs
+++ source/DataViewer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using AdonisUI.Controls;
+﻿using System.Windows;
+using AdonisUI.Controls;
 using DataViewer.ViewModels;
 
 namespace DataViewer;
@@ -13,5 +14,29 @@ public partial class MainWindow : AdonisWindow
         InitializeComponent();
 
         this.DataContext = viewModel;
+    }
+
+    private void MainWindow_DragEnter(object sender, System.Windows.DragEventArgs e)
+    {
+        ((MainViewModel)this.DataContext).DragEnterCommand.Execute(e);
+
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            e.Effects = DragDropEffects.Copy;
+        }
+        else
+        {
+            e.Effects = DragDropEffects.None;
+        }
+    }
+
+    private void MainWindow_DragLeave(object sender, DragEventArgs e)
+    {
+        ((MainViewModel)this.DataContext).DragLeaveCommand.Execute(e);
+    }
+
+    private void MainWindow_Drop(object sender, System.Windows.DragEventArgs e)
+    {
+        ((MainViewModel)this.DataContext).DropCommand.Execute(e);
     }
 }
--- source/DataViewer/ViewModels/MainViewModel.cs
+++ source/DataViewer/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace DataViewer.ViewModels;
 
@@ -6,4 +8,33 @@ public partial class MainViewModel() : ObservableObject
 {
     [ObservableProperty]
     private string _title = "DataViewer by lk-code";
+
+    [ObservableProperty]
+    private bool _isDragDropUIVisible = false;
+
+    [ObservableProperty]
+    private string _loadedFilePath = "";
+
+    [RelayCommand]
+    private void DragEnter(System.Windows.DragEventArgs e)
+    {
+        this.IsDragDropUIVisible = true;
+    }
+
+    [RelayCommand]
+    private void DragLeave(System.Windows.DragEventArgs e)
+    {
+        this.IsDragDropUIVisible = false;
+    }
+
+    [RelayCommand]
+    private void Drop(System.Windows.DragEventArgs e)
+    {
+        this.IsDragDropUIVisible = false;
+
+        string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+        string targetFile = files[0];
+
+        this.LoadedFilePath = targetFile;
+    }
 }
